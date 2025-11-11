0389bbecc8620039889a65bdfdf2e485c96b3fba
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-11 17:25:13 +0530

    day1

--- day 1/new.py
+++ day 1/new.py
@@ -1,1 +1,44 @@
-print("Heloo World from new.py!")
+# print("Heloo World from new.py!")
+
+# operations a two numbers 
+
+
+# a = 76
+# b = 15
+# c = a + b
+# print("addition:",c)
+# c = a * b
+# print("multiplication:",c)
+# c = a / b
+# print("division:",c)
+# c = a - b
+# print("subtraction:",c)
+
+# # operations a two numbers by user input
+
+
+# a = int(input("Enter a value: "))
+# b = int(input("Enter b value: "))
+# c = a + b
+# print("addition:",c)
+# c = a * b
+# print("multiplication:",c)
+# c = a / b
+# print("division:",c)
+# c = a - b
+# print("subtraction:",c)
+
+# area of rectangle
+
+# len = int(input("Enter length: "))
+# bredth = int(input("Enter bredth: "))
+
+# area = len * bredth
+
+# print("Area of rectangle is:",area)
+
+# area of circle
+import math
+radius = float(input("Enter radius of circle: "))
+area = math.pi * radius ** 2
+print("Area of circle is:",area)
