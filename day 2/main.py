fe82a60a7ee40020b0fa3d56332fc6a2bd4297d8
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-12 16:56:14 +0530

    adding some questions

--- day 2/main.py
+++ day 2/main.py
@@ -1,2 +1,89 @@
-# condition statements in Python
+# # condition statements in Python
 
+# # age = int(input("Enter your age: "))
+# # if age < 18:
+# #     print("Sorry, you are not eligible to vote.")
+# # else:
+# #     print("Yes you can vote!")
+
+# # number = int(input("Enter a number: "))
+# # if number % 2 == 0:
+# #     print(f"{number} is an even number.")
+# # else:
+# #     print(f"{number} is an odd number.")
+
+
+# # questions
+
+# # Q1
+# num = int(input("Enter a number: "))
+# if num > 0:
+#     print(f"{num} is a positive number.")
+# elif num < 0:
+#     print(f"{num} is a negative number.")
+# else:
+#     print("The number is zero.")
+
+# # Q2
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print(f"{number} is an even number.")
+# else:
+#     print(f"{number} is an odd number.")
+
+# # Q3
+
+# age = int(input("Enter your age: "))
+# if age < 18:
+#     print("Sorry, you are not eligible to vote.")
+# else:
+#     print("Yes you can vote!")
+
+# # Q4
+# number = int(input("Enter a number: "))
+# if number % 5 == 0:
+#     print(f"{number} is divisible by 5.")
+# else:
+#     print(f"{number} is not divisible by 5.")
+
+# # Q5
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# if num1 > num2:
+#     print(f"{num1} is greater than {num2}.")
+# elif num2 > num1:
+#     print(f"{num2} is greater than {num1}.")
+# else:
+#     print("Both numbers are equal.")
+
+# # Q6
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# if num1 >= num2 and num1 >= num3:
+#     print(f"{num1} is the largest number.")
+# elif num2 >= num1 and num2 >= num3:
+#     print(f"{num2} is the largest number.")
+# else:
+#     print(f"{num3} is the largest number.")
+
+# Q7
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# char = input("Enter a character: ").lower()
+# if char in vowels:
+#     print(f"{char} is a vowel.")
+# else:
+#     print(f"{char} is not a vowel its a consonant.")
+
+
+# Q8
+
+unit = int(input("Enter the number of electricity units consumed: "))
+if unit <= 100:
+    bill = unit * 5
+elif unit > 100 and unit < 200:
+    bill = unit * 7
+elif unit >= 200:
+    bill = unit * 10
+print(f"Your electricity bill is: {bill} units.")
