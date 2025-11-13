19e59f69f9577cb9914a4506f0867cba7c44db3a
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-13 17:37:28 +0530

    day 3 add some questions

--- day 3/main.py
+++ day 3/main.py
@@ -33,3 +33,68 @@
 #         continue
 #     print(i)
 
+
+# Q1
+
+# for i in range(1, 11):
+#     print(i)
+
+# Q2
+# count = 0
+# while count < 20:
+#     count += 1
+#     if count % 2 == 0:
+#         continue
+#     print(count)
+
+# # Q3
+# for i in range (1, 11):
+#     if i % 2 == 0:
+#         print("even", i)
+#     else:
+#         print("odd", i)
+
+# # Q4
+# total = 0
+# sumofN = 10
+# while sumofN > 0:
+#     total += sumofN
+#     sumofN -= 1
+# print("Sum is:", total)
+
+# # Q5
+# for i in range(1, 11):
+#     print("2 *", i, "=", 2 * i)
+
+# Q6
+# count = 10
+# while count > 0:
+#     print(count)
+#     count -= 1
+
+# Q7
+
+# for i in range(1,6):
+#     print("sqares of Numbers", i ,"to 5",i**2)
+
+# Q8
+
+# for i in range(1, 51):
+#     if i % 5 == 0:
+#         print("Numbers are divisible by 5",i)
+
+#Q9
+
+# count = 0
+# while count < 50:
+#     count += 1
+#     if count % 2 != 0:
+#         continue
+#     print("Even Numbers using while loop:", count)
+
+#Q10
+
+# num = int(input("Enter a number to reverse a number: "))
+# while num > 0:
+#     print(num)
+#     num -= 1
