34e3358ce6bdca515a8bf4bc8d41dba5524f48d5
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-13 16:39:09 +0530

    adding some questions

---
+++ day 3/main.py
@@ -0,0 +1,35 @@
+# loops
+
+# only two types of loops in python for and while
+
+# for loop used when number of iterations is known
+
+# name = "Kunal"
+# for i in range(5):
+#     print(name)
+
+# for i in range(1, 6):
+#     print(i)
+
+# for i in range(1, 11):
+#     print("2 *", i, "=", 2 * i)
+
+# while loop used when number of iterations is not known
+
+# count = 0
+# while count < 5:
+#     print("Hello World")
+#     count += 1
+
+# total = 0
+# num = 1
+# while num <= 5:
+#     total += num
+#     num += 1
+# print("Sum of Total:", total)
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
