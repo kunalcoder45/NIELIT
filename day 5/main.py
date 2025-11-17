f16fd0916c0b383a366470837fa9e77be6b5d66e
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-17 17:21:39 +0530

    add codes

--- day 5/main.py
+++ day 5/main.py
@@ -1,5 +1,37 @@
 # Numpy Numeric Python Travis Oliphant created 2005 use Data Analysis Scientific Computing
 
 import numpy as np
-a = np.array([1, 2, 3])
-print("Numpy Array:", a)
+
+# creating list converting into array, 1D Array
+# a = np.array([[1, 2, 3, 4, 5]])
+# print("1D Array:\n", a)
+
+# # creating 2D Array
+# b = np.array([[1, 2, 3], [4, 5, 6]])
+# print("2D Array:\n", b)
+# print(b.shape)
+# print(b[1,2])
+
+# # creating 3D Array
+# c = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# print("3D Array:\n", c)
+# print(c.shape)
+
+# creating a 3 rows and 4 columns array
+# a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+# print("Array:\n", a)
+# print(a.shape)
+# print(a.ndim)
+# print(a.itemsize)
+# print(a.dtype)
+# print(a.size)
+# newVar = a.reshape(4,3)
+# print("Reshaped Array:\n", newVar)
+
+# creating a 3d array 3 rows and 4 columns and 2 depth
+
+# b = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]], [[13,14,15,16], [17,18,19,20], [21,22,23,24]]])
+# print("3D Array:\n", b)
+# print(a.shape)
+# print(a.ndim)
+# print(a.size)
