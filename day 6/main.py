54f3630f834681894042d87f56f53fcaffb85f35
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-18 16:46:22 +0530

    add new folder day 6 and code

---
+++ day 6/main.py
@@ -0,0 +1,31 @@
+# slicing in array
+
+import numpy as np
+
+a = np.array([[1, 2, 3, 10, 15, 4]])
+a.max()
+print("Original Array:")
+print(a)
+print(a.max())
+print(a.min())
+print(a.sum())
+print(a.mean())
+
+a = np.array([[1, 2, 3, 10, 15, 4],
+              [5, 6, 7, 11, 16, 8],
+              [9, 10, 11, 12, 17, 14]])
+print(a[2, 4])
+
+
+# 1.	Create two array of size (3, 3) and print their sum and multiplication.
+
+
+# array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# array2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+# sum_array = np.add(array1, array2)
+# product_array = np.multiply(array1, array2)
+# print("Sum of the two arrays:")
+# print(sum_array)
+# print("Product of the two arrays:")
+# print(product_array)
+
