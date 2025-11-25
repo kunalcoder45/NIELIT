080c28a2a0e253f93a4f5c92df941fcfbadacf1f
kunalcoder45 <kunalcoder45@gmail.com>
2025-11-25 16:45:51 +0530

    adding files

---
+++ day 10/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+d = {'one': pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+     'two': pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+}
+
+df = pd.DataFrame(d)
+print("First\n",df)
+print("Second\n",df['one'])
+print("Third\n",df['two'])
