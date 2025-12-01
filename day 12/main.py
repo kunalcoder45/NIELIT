261c4b0544323f342e3ebece5b7c1c0396f06d5d
kunalcoder45 <kunalcoder45@gmail.com>
2025-12-01 17:22:11 +0530

    adding file handling codes

---
+++ day 12/main.py
@@ -0,0 +1,17 @@
+# openFile = open("day 12\\file.txt", "r")
+# lines = openFile.read()
+# print(lines)
+# lines = openFile.tell()
+# openFile.close()
+# print(lines)
+
+
+# import pickle
+# myfile = open("day 12\\file.txt", "wb")
+# data = {"name": "Kunal", "age": 30, "city": "Jamshedpur"}
+# pickle.dump(data, myfile)
+# myfile.close()
+
+
+import os
+os.mkdir("day 12\\newfolder")
